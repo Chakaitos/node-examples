2202ef04e98fa97432ef28638c037e1d05961ca8
Jose Borja <chakaitos@gmail.com>
2015-12-15 11:27:44 -0500

    Adds ejs for as a view engine

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var port = process.env.PORT || 3000;
 
 app.use('/assets', express.static(__dirname + '/public'));
 
+app.set('view engine', 'ejs');
+
 app.use('/', function (req, res, next) {
   console.log('Request Url:' + req.url);
   next();
